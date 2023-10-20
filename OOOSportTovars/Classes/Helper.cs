df665c99c95d811a5cf49bcbdbe484b21dc48cd4
Small-lynx <lynxrufus2k4@gmail.com>
2023-10-20 18:02:49 +0300

    Создание класса для помощи обращения к БД

---
+++ OOOSportTovars/Classes/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOOSportTovars.Classes
+{
+    internal class Helper
+    {
+        // Объявление объекта
+        public static Model.SportTovarsEntities DB { get; set; }
+        public static List<Model.User> Users = new List<Model.User>();
+    }
+}
